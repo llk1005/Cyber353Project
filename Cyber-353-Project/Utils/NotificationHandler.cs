4df90a328c3b1752822cc838c98f81bfba9298bb
Zachary Freilino <zlf1001@sru.edu>
2021-12-06 09:27:46 -0500

    Added readonly to fields to fix warning messages

--- Cyber-353-Project/Utils/NotificationHandler.cs
+++ Cyber-353-Project/Utils/NotificationHandler.cs
@@ -11,9 +11,9 @@ namespace Cyber_353_Project
     static class NotificationHandler
     {
 
-        private static string from = "cyber353notificationsystem@gmail.com";
-        private static string user = "cyber353notificationsystem";
-        private static string pass = "Cyb3r353-$";
+        private static readonly string from = "cyber353notificationsystem@gmail.com";
+        private static readonly string user = "cyber353notificationsystem";
+        private static readonly string pass = "Cyb3r353-$";
 
         public static bool SendMessage(string message)
         {
